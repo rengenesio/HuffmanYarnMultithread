8154e06131f7c66190d01fe6309b9d05817b100c
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-20 23:52:41 -0200

    Testing action queue

--- src/br/ufrj/ppgi/huffmanyarnmultithread/encoder/Encoder.java
+++ src/br/ufrj/ppgi/huffmanyarnmultithread/encoder/Encoder.java
@@ -2,6 +2,8 @@ package br.ufrj.ppgi.huffmanyarnmultithread.encoder;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 
@@ -27,8 +29,10 @@ public final class Encoder {
 	
 	// Array of byte arrays (each byte array represents a input split byte sequence) 
 	private byte[][] memory;	
+	
 	// Associates a inputSplit with a index in memory matrix
-	private int[] memoryPartMap;
+	//private int[] memoryPartMap;
+	private Map<Integer, Integer> memoryPartMap; 
 	
 	private int[][] frequencyMatrix;
 	private long[] totalFrequencyArray;
@@ -41,7 +45,8 @@ public final class Encoder {
 	private int maxThreads = 8;
 	private int numTotalThreads = 1;
 	
-	private LinkedBlockingQueue<String> actionQueue = new LinkedBlockingQueue<String>();
+	private LinkedBlockingQueue<Integer> diskActionQueue = new LinkedBlockingQueue<Integer>();
+	private LinkedBlockingQueue<Integer> memoryActionQueue = new LinkedBlockingQueue<Integer>();
 	
 	public Encoder(String[] args) {
 		this.conf = new Configuration();
@@ -62,9 +67,12 @@ public final class Encoder {
 		
 		this.numTotalInputSplits = this.inputSplitCollection.size();
 		
+		this.memoryPartMap = new HashMap<Integer, Integer>(numTotalInputSplits);
+		
 		this.masterHostName = args[2];
 		this.numTotalContainers = Integer.parseInt(args[3]);
 	}
+	
 	
 	public void encode() throws IOException, InterruptedException {
 		if(this.numTotalInputSplits >= this.maxThreads) {
@@ -75,51 +83,63 @@ public final class Encoder {
 
 		chunksToMemory();
 		
-		while(actionQueue.isEmpty() == false) {
-			String action = actionQueue.take();
-			System.out.print(action);
-//			System.out.print("  ->  ");
-//			
-//			String[] s = StringUtils.split(action, ' ');
-//			int memoryPart = -1;
-//			for(int i = 0 ; i < numTotalInputSplits ; i++) {
-//				if(Integer.parseInt(s[1]) == this.memoryPartMap[i]) {
-//					memoryPart = i;
-//				}
-//			}
-//			
-//			StringBuilder stringBuilder = new StringBuilder();
-//			stringBuilder.append((char)memory[memoryPart][0]);
-//			stringBuilder.append((char)memory[memoryPart][1]);
-//			stringBuilder.append((char)memory[memoryPart][2]);
-//			stringBuilder.append((char)memory[memoryPart][3]);
-//			stringBuilder.append((char)memory[memoryPart][4]);
-//			System.out.println(stringBuilder.toString());
+		System.out.println("Memória:");
+		while(memoryActionQueue.isEmpty() == false) {
+			int chunk = memoryActionQueue.take();
+			System.out.println(chunk);
+		}
+		
+		System.out.println("\nDisco:");
+		while(diskActionQueue.isEmpty() == false) {
+			int chunk = diskActionQueue.take();
+			System.out.println(chunk);
 		}
 		
 //		ArrayList<Thread> threadCollection = new ArrayList<Thread>();
 //		for(int i = 0 ; i < numTotalThreads ; i++) {
-//			Thread t = new Thread(new Runnable() {
+//			Thread thread = new Thread(new Runnable() {
 //				
 //				@Override
 //				public void run() {
 //					Semaphore actionQueueSemaphore = new Semaphore(1);
 //					
 //					while(true) {
-//						actionQueueSemaphore.acquire();
-//						if(actionQueue.isEmpty() == false) { return; }
-//						String action = actionQueue.take();
+//						try {
+//							actionQueueSemaphore.acquire();
+//						} catch (InterruptedException e) {
+//							e.printStackTrace();
+//						}
+//
+//						String action = null;
+//						do {
+//							try {
+//								action = actionQueue.take();
+//							} catch (InterruptedException e) {
+//								e.printStackTrace();
+//							}
+//							
+//						}while(action)
+//						
+//						
+//						
 //						actionQueueSemaphore.release();
 //
-//						if(action.startsWith(prefix))
+//						//Thread.currentThread().getId();
 //						
 //						
 //					}
 //				}
-//			}
+//			});
 //			
-//			t.start();
+//			threadCollection.add(thread);
+//			thread.start();
 //		}
+//		
+//		// Bloqueia até que todas as threads tenham terminado a contagem dos caracteres
+//		for(Thread thread : threadCollection) {
+//			thread.join();
+//		}
+	
 //		
 //		memoryToFrequency();
 //
@@ -323,40 +343,60 @@ public final class Encoder {
 		FSDataInputStream f = fs.open(path);
 		
 		memory = new byte[this.numTotalInputSplits][];
-		memoryPartMap = new int[this.numTotalInputSplits];
+		//memoryPartMap = new int[this.numTotalInputSplits];
 
-		boolean memoryFull = false;
+
 		int i = 0;
+		boolean memoryFull = false;
 		while(i < this.inputSplitCollection.size()) {
-			
 			// Adiciona mais 1 no tamanho do split pra poder adicionar na memória o marcador de fim do bloco da compressão (EOF)
 			inputSplitCollection.get(i).length++;
 			
 			if(memoryFull == false) {
 				try {
+					// Tenta alocar espaço na memória para este chunk. Se não conseguir, um erro será lançado e capturado
 					memory[i] = new byte[(int) inputSplitCollection.get(i).length];
+					
+					// Lê o chunk do disco para o bloco de memória que acabou de ser alocado
 					f.read(inputSplitCollection.get(i).offset, memory[i], 0, inputSplitCollection.get(i).length);
+					
+					// Adiciona o marcador de final de bloco na compressão
 					memory[i][inputSplitCollection.get(i).length - 1] = 0;
-					// TODO: consertar exception que está sendo lançada aqui
-					memoryPartMap[i] = inputSplitCollection.get(i).part;
-					actionQueue.add(new String("m " + inputSplitCollection.get(i).part));
-					i++;
+					
+					// Mapeia o número do chunk lido para um índice do array da memória
+					memoryPartMap.put(inputSplitCollection.get(i).part, i);
+					
+					// Adiciona este chunk na lista de ações a serem feitas da memória
+					memoryActionQueue.add(inputSplitCollection.get(i).part);
+					
 				}
 				catch(Error error) {
-					System.out.println("Memoria encheu!!!!");
-					System.err.println("Memoria encheu!!!!");
+					// Adiciona este chunk na lista de ações a serem feitas do disco
+					diskActionQueue.add(inputSplitCollection.get(i).part);
+					
+					// Seta a variável de controle para que ele não tente alocar mais espaço na memória
 					memoryFull = true;
 				}
 			}
 			else {
-				memoryPartMap[i] = inputSplitCollection.get(i).part;
-				actionQueue.add(new String("d " + inputSplitCollection.get(i).part));
-				i++;
+				// Adiciona este chunk na lista de ações a serem feitas do disco
+				diskActionQueue.add(inputSplitCollection.get(i).part);
 			}
 		}
 	}
 	
-//	public void memoryToFrequency(int chunk) throws IOException {
+//	public void chunkToFrequency(int threadId, int chunk) throws IOException {
+//		
+//		
+//		if(this.memoryPartMap.get(chunk) == null) {
+//			// Esta parte não está na memória, está no disco
+//		}
+//		else {
+//			// Esta parte está na memória
+//			for (int i = 0; i < inputLength; i++) {
+//				frequencyArray[(memory[i] & 0xFF)]++;
+//			}
+//		}
 //		
 //		if(this.inputOffset == 0) {
 //			this.totalFrequency = new long[256];
@@ -379,7 +419,7 @@ public final class Encoder {
 //        System.out.println("------------------------------");
 //        */
 //	}
-	
+//	
 //	
 //	public void frequencyToNodeArray() {
 //		this.nodeArray = new NodeArray((short) this.symbols);
