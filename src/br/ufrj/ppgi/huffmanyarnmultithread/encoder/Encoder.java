11c3912bc3e1726815b23ecc78b7697701c3fb16
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-21 03:49:17 -0200

    Testing action queue

--- src/br/ufrj/ppgi/huffmanyarnmultithread/encoder/Encoder.java
+++ src/br/ufrj/ppgi/huffmanyarnmultithread/encoder/Encoder.java
@@ -263,6 +263,7 @@ public final class Encoder {
 							}
 							
 							totalReadBytes += readBytes;
+							System.err.println("TotalReadBytes: " + totalReadBytes);
 						}
 					}
 					else {
