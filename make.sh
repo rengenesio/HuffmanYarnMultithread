83d2a43e224fc65b46458dc7aacb3bb24e01cc88
Renan Iglesias <renan.iglesias@ufrj.br>
2015-02-23 02:12:16 -0300

    Script refactor

--- make.sh
+++ make.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-mkdir bin
-mkdir release
-
 javac src/br/ufrj/ppgi/huffmanyarnmultithread/*.java src/br/ufrj/ppgi/huffmanyarnmultithread/encoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithread/decoder/*.java src/br/ufrj/ppgi/huffmanyarnmultithread/yarn/*.java -d bin
 
 ant
